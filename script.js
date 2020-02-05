b2fabda501ad6e0bcf5a39954afb7fe6f3274cbe
Y0ss-Please <casey.elzinga@gmail.com>
2020-02-05 16:02:33 -0700

    Building basic site functionality

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
 ///const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 ///const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('overlay').style.opacity = '0';
+});
+  
 function scrollFunction() {
     const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    const aspect = (vw >= vh) ? true : false ;
     const scroll = document.documentElement.scrollTop;
-    const initialFontSize = vh * 0.45;
+    let atMinimum = false;
 
+    const initialFontSize = aspect ? vh * 0.45 : vw * 0.38;
+    const minimumFontSize = initialFontSize/5.4;
     let newFontSize = initialFontSize - (scroll);
-    if (newFontSize < (vh/12    )) newFontSize = vh/12;
+    if (newFontSize < (minimumFontSize)) {
+        newFontSize = minimumFontSize;
+        atMinimum = true;
+    } else { 
+        atMinimum = false;
+    }
     document.getElementById('logo').style.fontSize = newFontSize+'px';
 
-    const initialNavbarSize = vh * 0.435;
+    const initialNavbarSize = aspect ? vh * 0.435 : vw * 0.37;
+    const minimumNavbarSize = initialNavbarSize/3.5;
     let newNavbarSize = initialNavbarSize - (scroll);
-    if (newNavbarSize < (vh/8)) newNavbarSize = vh/8;
+    if (atMinimum) newNavbarSize = minimumNavbarSize;
     document.getElementById('navbar').style.height = newNavbarSize+'px';
 
-    const newSpacerSize = vh - (scroll);
-    document.getElementById('navbar-spacer').style.height = newSpacerSize+'px';
-
-    document.getElementById('herobox').style.transform = `translate(${scroll} 0px)`;
+    const contact = document.getElementById('contact');
+    if (scroll < 350){
+        contact.style.bottom = "-100%";
+    } else {
+        contact.style.bottom = "0";
+    }
 } 
 
 window.onscroll = function() {scrollFunction()};
+window.onresize = function() {scrollFunction()};
+
+scrollFunction();
